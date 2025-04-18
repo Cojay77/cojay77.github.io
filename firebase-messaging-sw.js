6ca016093660e955854351e8edf353f3c89c1bf3
Cojay77 <Cojay77@users.noreply.github.com>
2025-04-18 07:54:31 +0000

    deploy: Cojay77/DNDNB@38a718bbdf69ec92f658f242f36d6bf86ba8a711

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
+// web/firebase-messaging-sw.js
+
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyA2hHdz8fse7fMJP8ygu05LtPUDbAqPKVQ",
   authDomain: "dispo-des-bros.firebaseapp.com",
   projectId: "dispo-des-bros",
   messagingSenderId: "604239799472",
-  appId: "1:604239799472:web:c1eb4d25703e47f6c88775"
+  appId: "1:604239799472:android:7162ed4aeaae3665c88775",
 });
 
 const messaging = firebase.messaging();
+
+// Background
+messaging.onBackgroundMessage((payload) => {
+  console.log('📦 Message reçu en arrière-plan : ', payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/DDB/icons/Icon-192.png'
+  });
+});
