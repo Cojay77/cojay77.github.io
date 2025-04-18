e81aa9e35a9eb861ed9e957963fcddc7741f2ad8
Cojay77 <Cojay77@users.noreply.github.com>
2025-04-18 14:15:58 +0000

    deploy: Cojay77/DNDNB@0e728bc59f9e273b8243904abda4a639933513bf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"index.html": "c880b18e95e484fbcee1bea69b8ef3d1",
-"/": "c880b18e95e484fbcee1bea69b8ef3d1",
+"index.html": "c875b6d3b4a29eb4e2d47eacf6accded",
+"/": "c875b6d3b4a29eb4e2d47eacf6accded",
 "assets/NOTICES": "be6703ab2ca8a0f7ae20827329cac1e5",
 "assets/assets/logo.png": "b6d6506af2814c7bdcc441da9e2b91cf",
 "assets/assets/fonts/UncialAntiqua-Regular.ttf": "60b9a246b9e409e99e777449f68c39ea",
@@ -37,7 +37,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "894cc400c389b6868c30d4d7b3978859",
 "manifest.json": "834c6725c181760cca8bd9998a841c4b",
 "favicon.png": "894cc400c389b6868c30d4d7b3978859",
-"flutter_bootstrap.js": "35e40f1aa7dd9257669328748bd3eeab"};
+"flutter_bootstrap.js": "0f87a51375c59c878a101bb4bef18e11"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
